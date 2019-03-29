e1d4a4f31359495bb0ecc66f6342393d4d79634b
Andrew Waters <waters@me.com>
2019-03-29 17:56:55 +0000

    Streamline

--- go/codeclimate/entrypoint.sh
+++ go/codeclimate/entrypoint.sh
@@ -33,9 +33,11 @@ echo $GIT_BRANCH
 
 # echo "cc-test-reporter before-build"
 #cc-test-reporter before-build
+echo "Running before build"
+
+cc-test-reporter before-build
 
 echo "Running go test with coverage"
-
 for pkg in $(go list ./... | grep -v main); do
     go test -coverprofile=$(echo $pkg | tr / -).cover $pkg
 done
@@ -43,20 +45,22 @@ echo "mode: set" > c.out
 grep -h -v "^mode:" ./*.cover >> c.out
 rm -f *.cover
 
+./cc-test-reporter after-build
+
 ls -la
 
 cat c.out
 
-cc-test-reporter format-coverage -c.out -t gocov
+# cc-test-reporter format-coverage -t gocov
 
-ls -la
+# ls -la
 
-cd coverage
+# cd coverage
 
-ls -la
+# ls -la
 
 
-cc-test-reporter upload-coverage
+# cc-test-reporter upload-coverage
 
 
 # echo "Running ./cc-test-reporter after-build"
