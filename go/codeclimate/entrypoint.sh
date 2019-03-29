a20ca8b92835938aa9f392fb25f8209b2912d635
Andrew Waters <waters@me.com>
2019-03-29 17:53:52 +0000

    Test packages

--- go/codeclimate/entrypoint.sh
+++ go/codeclimate/entrypoint.sh
@@ -35,10 +35,13 @@ echo $GIT_BRANCH
 #cc-test-reporter before-build
 
 echo "Running go test with coverage"
-go test -coverprofile=c.out
 
-echo "Tests completed"
+for pkg in $(go list ./... | grep -v main); do
+    go test -coverprofile=$(echo $pkg | tr / -).cover $pkg
+done
 echo "mode: set" > c.out
+grep -h -v "^mode:" ./*.cover >> c.out
+rm -f *.cover
 
 ls -la
 
