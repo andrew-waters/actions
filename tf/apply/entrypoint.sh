1043cac712f503de9b6725cbc92c8f447dc738b7
Andrew Waters <waters@me.com>
2019-05-22 09:34:03 +0100

    Rely on merge filter action

--- tf/apply/entrypoint.sh
+++ tf/apply/entrypoint.sh
@@ -2,40 +2,26 @@
 set -e
 cd "${TF_ACTION_WORKING_DIR:-.}"
 
+set +e
+OUTPUT=$(sh -c "terraform apply -no-color -input=false -auto-approve $*" 2>&1)
+SUCCESS=$?
+echo "$OUTPUT"
+set -e
 
-merged=$(jq --raw-output .pull_request.merged "$GITHUB_EVENT_PATH")
-
-cat "$GITHUB_EVENT_PATH"
-
-if [ "$merged" == "true" ]; then
-
-    set +e
-    OUTPUT=$(sh -c "terraform apply -no-color -input=false -auto-approve $*" 2>&1)
-    SUCCESS=$?
-    echo "$OUTPUT"
-    set -e
-
-    if [ $SUCCESS -eq 0 ]; then
-        exit 0
-    fi
-
-    if [ "$TF_ACTION_COMMENT" = "1" ] || [ "$TF_ACTION_COMMENT" = "false" ]; then
-        exit $SUCCESS
-    fi
-
-    COMMENT="#### \`terraform apply\` Failed
-    \`\`\`
-    $OUTPUT
-    \`\`\`"
-    PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
-    COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
-    curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
-
-    exit $SUCCESS
-
-else
-
-    echo "Not merged, exiting"
+if [ $SUCCESS -eq 0 ]; then
     exit 0
-
 fi
+
+if [ "$TF_ACTION_COMMENT" = "1" ] || [ "$TF_ACTION_COMMENT" = "false" ]; then
+    exit $SUCCESS
+fi
+
+COMMENT="#### \`terraform apply\` Failed
+\`\`\`
+$OUTPUT
+\`\`\`"
+PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
+COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
+curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
+
+exit $SUCCESS
